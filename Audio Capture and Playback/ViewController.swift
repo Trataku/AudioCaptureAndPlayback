774cf28e6c6ddd2fed6405865a8b9d968641096e
Dylan Mouser <codecruncher@mwc-052084.dhcp.missouri.edu>
2019-04-12 19:21:33 -0500

    Initial Commit

---
+++ Audio Capture and Playback/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Audio Capture and Playback
+//
+//  Created by Dylan Mouser on 4/12/19.
+//  Copyright © 2019 Dylan Mouser. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
